7cb261f4670b0666f60aea23376cb996e4e7a49d
blohamen <staver82@gmail.com>
2018-02-21 21:36:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
+  var arr = equation.split(' ');
+  var a = parseInt(arr[0]);
+  var b = (arr[3] === '-') ? -arr[4] : arr[4]; 
+  var c = (arr[7] === '-') ? -arr[8] : arr[8]; 
+  var discriminant = (b*b) - (4*a*c);
+   var x1 = Math.round(-b + (Math.sqrt(discriminant)))/(2*a);
+   var x2 = Math.round(-b - (Math.sqrt(discriminant)))/(2*a);
+  arr.length = 0;
+  arr.push(x2);
+  arr.push(x1);
+  arr.sort(function(a,b){
+    return a-b;
+  });
+  return arr;
+
   // your implementation
 }
